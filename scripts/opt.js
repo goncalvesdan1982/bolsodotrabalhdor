92f13112779d242ca83ce9deed0605b8ad8be7e0
Daniel Gonçalves <goncalvesdan1982@users.noreply.github.com>
2026-07-13 20:53:15 -0300

    chore: remove script de otimizacao

--- scripts/opt.js
+++
@@ -1,6 +0,0 @@
-const sharp = require('sharp');
-const input = 'public/novo-logo-bolso.jpg';
-const output = 'public/novo-logo-bolso.webp';
-sharp(input).webp({ quality: 85, effort: 6 }).toFile(output).then(info => {
-  console.log('WebP: ' + (info.size / 1024).toFixed(0) + ' KB');
-}).catch(e => console.log(e.message));
